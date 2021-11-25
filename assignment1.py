1e8718cbe4b92a95839df5ede284df7bc4239656
Caleb <jiale.hou@my.jcu.edu.au>
2021-11-25 13:39:59 +0800

    Add menu, and load movie detail into list.

--- assignment1.py
+++ assignment1.py
@@ -5,11 +5,25 @@ Date started:
 GitHub URL:
 """
 
+print("Movies to watch 1.0 - by Jiale Hou")
+
+MENU = """L - List movie
+A - Add new movie
+W - Watch a movie
+Q - Quit"""
+
 
 def main():
-    """..."""
-    print("Movies To Watch 1.0 - by <Your Name>")
+    movie_list = load_movies()
+    print(movie_list)
 
 
-if __name__ == '__main__':
-    main()
+def load_movies():
+    movies = open("movies.csv", 'r')
+    movie_list = []
+    for movie in movies:
+        movie_list.append(movie)
+    return movie_list
+
+
+main()
